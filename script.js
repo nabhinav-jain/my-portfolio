3b46d815660e47d21aeb7674e93c8c5cef630b47
abhinav <jabhinav041@gmail.com>
2025-01-30 20:17:26 +0530

    optmised js code

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
     const lmProject = document.getElementById("lm-project");
     const bicciProject = document.getElementById("bicci-project");
-  
 
-    function animateElement(element, fromX, toX) {
-      element.style.opacity = 0;
-      element.style.transform = `translateX(${fromX}%)`;
-      element.style.transition = "transform 1s ease-out, opacity 2s ease-out"; // Define transition here
-  
-      // Trigger reflow (important!)
-      element.offsetWidth;
-  
-      setTimeout(() => {
-        element.style.opacity = 1;
-        element.style.transform = `translateX(${toX}%)`;
-      }, 500); // Delay before animation starts
+    function animateElement(element, fromX) {
+        
+
+        const animation = element.animate([
+            {  transform: `translateX(${fromX}%)` }, 
+            { opacity: 1, transform: "translateX(0%)" } 
+        ], {
+            duration: 1000, 
+            easing: "ease-out",
+            fill: "forwards" 
+        });
+
+        return animation;
     }
-  
+
     const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const target = entry.target;
-  
-          if (target.id === "lm-project") {
-              animateElement(target, -100, 0); // Slide from left
-          } else if (target.id === "bicci-project") {
-              animateElement(target, 100, 0); // Slide from right
-          }
-  
-          observer.unobserve(target);
-        }
-      });
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const target = entry.target;
+
+                if (target.id === "lm-project") {
+                    animateElement(target, -100); 
+                } else if (target.id === "bicci-project") {
+                    animateElement(target, 100); 
+                }
+
+                observer.unobserve(target); 
+            }
+        });
     }, { threshold: 0.15 });
-  
+
     observer.observe(lmProject);
     observer.observe(bicciProject);
-  });
+
